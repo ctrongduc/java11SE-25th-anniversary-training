a4456128964db414d7c115fefe34fa4684f9baf4
Duc Chu <chutrongduc@gmail.com>
2021-04-20 21:33:30 -0500

    Updated with Java Stream API and Exception

--- src/main/java/labs/pm/data/ProductManager.java
+++ src/main/java/labs/pm/data/ProductManager.java
@@ -7,8 +7,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class ProductManager {
+    private final Logger logger = Logger.getLogger(ProductManager.class.getName());
+
     private Map<Product, List<Review>> products = new HashMap<>();
     private ResourceFormatter formatter;
 
@@ -57,25 +62,22 @@ public class ProductManager {
         products.remove(product, reviews);
         reviews.add(new Review(rating, comment));
 
-        int sum = 0;
-        for (Review review : reviews) {
-            sum += review.getRating().ordinal();
-        }
+        int avg = (int) Math.round(reviews.stream()
+                .mapToInt(r -> r.getRating().ordinal())
+                .average()
+                .orElse(0));
 
-        product = product.applyRating(Rateable.convert(Math.round((float)sum/reviews.size())));
-        products.put(product, reviews);
+        products.put(product.applyRating(avg), reviews);
         return product;
     }
 
     public Product findProduct(int id) {
-        Product result = null;
-        for (Product product : products.keySet()) {
-            if (product.getId() == id) {
-                result = product;
-                break;
-            }
-        }
-        return result;
+        return products.keySet()
+                    .stream()
+                    .filter(p -> p.getId() == id)
+                    .findFirst()
+                    .orElseThrow(() -> new ProductManagerException("Product with id "+ id + "not found"));
+                    //.orElseGet(null);
     }
 
     public void printProductReport(int id) {
@@ -84,30 +86,57 @@ public class ProductManager {
 
     public void printProductReport(Product product) {
         StringBuilder txt = new StringBuilder();
-        txt.append(formatter.formatProduct(product));
-        txt.append("\n");
         List<Review> reviews = products.get(product);
         Collections.sort(reviews);
-        for (Review review : reviews) {
-            txt.append(formatter.formatReview(review));
-            txt.append("\n");
-        }
+        txt.append(formatter.formatProduct(product));
+        txt.append("\n");
+//        for (Review review : reviews) {
+//            txt.append(formatter.formatReview(review));
+//            txt.append("\n");
+//        }
         if (reviews.isEmpty()) {
-            txt.append(formatter.getText("no.reviews"));
-            txt.append("\n");
+            txt.append(formatter.getText("no.reviews")).append('\n');
+        } else {
+            // 1st option, this might have problem with parallel mode
+            reviews.stream().forEach(r -> txt.append(formatter.formatReview(r)).append('\n'));
+            // 2nd option, using map, this works fine with parallel,
+            // txt.append(reviews.stream().map(r -> formatter.formatReview(r) + '\n').collect(Collectors.joining()));
         }
         System.out.println(txt.toString());
     }
 
-    public void printProducts(Comparator<Product> sorter) {
-        List<Product> productList = new ArrayList<>(products.keySet());
-        productList.sort(sorter);
+    public void printProducts(Predicate<Product> filter, Comparator<Product> sorter) {
         StringBuilder txt = new StringBuilder();
-        for(Product product : productList) {
-            txt.append(formatter.formatProduct(product));
-            txt.append("\n");
-        }
+        products.keySet()
+                .stream()
+                .sorted(sorter)
+                .filter(filter)
+                .forEach(p -> txt.append(formatter.formatProduct(p)).append('\n'));
         System.out.println(txt);
+    }
+
+    public void printProducts(Comparator<Product> sorter) {
+        StringBuilder txt = new StringBuilder();
+        products.keySet()
+                .stream()
+                .sorted(sorter)
+                .forEach(p -> txt.append(formatter.formatProduct(p)).append('\n'));
+        System.out.println(txt);
+    }
+
+    public Map<String, String> getDiscount() {
+        return products.keySet()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                product -> product.getRating().getStars(),
+                                Collectors.collectingAndThen(
+                                        Collectors.summingDouble(product -> product.getDiscount().doubleValue()),
+                                        discount -> formatter.moneyFormat.format(discount)
+                                )
+                        )
+                );
+
     }
 
     public static class ResourceFormatter {
